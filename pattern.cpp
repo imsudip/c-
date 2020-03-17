4eabdcc4fe7c4c5aa9f61c7088ce84bb3879b3f3
imsudip <sudipghosh9333@gmail.com>
2020-03-17 21:33:18 +0530

    added new

--- pattern.cpp
+++ pattern.cpp
@@ -23,37 +23,8 @@ void printMatrix(){
     }
 }
 void maze(){
-   
-    for(int k=0;k<=n/2+1;k+=2){
-        for(int j=k;j<n-k;j++){
-            a[k][j]=1;
-            a[j][k]=1;
-        }
-    }
-    // for(int k=n-2;k>=n/2;k-=2){
-    //     for(int j=k;j>0;j--){
-    //         a[k][j]=1;
-    //         a[j][k]=1;
-    //     }
-    // }
-    int t=n;
-    for(int i=2;n-i!=0;i+=2){
-        for(int j=i;j<=t;j++){
-            a[j][n-i]=1;
-
-        }
-        t-=2;
-    }
-    for(int i=2;n-i!=0;i+=2){
-        for(int j=i;j<=t;j++){
-            a[n-i][j]=1;
-
-        }
-        t-=2;
-    }
     
 }
-
 int main(){
   
     cout<<"enter dimension: ";
