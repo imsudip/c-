c7f9a90fa99c766a934413d5d684248035dfd4bb
imsudip <sudipghosh9333@gmail.com>
2020-03-24 00:03:01 +0530

    a

--- coronaPrime.cpp
+++ coronaPrime.cpp
@@ -1,30 +1,6 @@
 #include<iostream>
  using namespace std;
 
-bool isPrime(int n)  
-{  
-    if (n <= 1)  return false;  
-    if (n <= 3)  return true;  
-    if (n%2 == 0 || n%3 == 0) return false;  
-    for (int i=5; i*i<=n; i=i+6)  
-        if (n%i == 0 || n%(i+2) == 0)  
-           return false;  
-    return true;  
-} 
-int nextPrime(int N) 
-{ 
-    if (N <= 1) 
-        return 2; 
-    int prime = N; 
-    bool found = false; 
-    while (!found) { 
-        prime++; 
-  
-        if (isPrime(prime)) 
-            found = true; 
-    } 
-    return prime; 
-}  
 
 int main(){
     int x,y,t;
@@ -53,3 +29,27 @@ int main(){
         cout<<"\n";
     }
 }
+bool isPrime(int n)  
+{  
+    if (n <= 1)  return false;  
+    if (n <= 3)  return true;  
+    if (n%2 == 0 || n%3 == 0) return false;  
+    for (int i=5; i*i<=n; i=i+6)  
+        if (n%i == 0 || n%(i+2) == 0)  
+           return false;  
+    return true;  
+} 
+int nextPrime(int N) 
+{ 
+    if (N <= 1) 
+        return 2; 
+    int prime = N; 
+    bool found = false; 
+    while (!found) { 
+        prime++; 
+  
+        if (isPrime(prime)) 
+            found = true; 
+    } 
+    return prime; 
+}  
