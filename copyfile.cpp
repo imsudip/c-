dca1c47dbf16bfb0e58975ee26d4cf81b2722d2c
imsudip <sudipghosh9333@gmail.com>
2020-06-05 19:23:50 +0530

    added copyfile

---
+++ copyfile.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    ifstream fin;
+    ofstream fout;
+    fin.open("one.txt");
+    fout.open("two.txt");
+    char c;
+    fin >> c;
+    while (!fin.eof())
+    {
+
+        if (c != ' ')
+            fout << c;
+        fin >> c;
+    }
+    fin.close();
+    fout.close();
+    return 0;
+}
